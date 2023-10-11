c94a8e47f372cd1269b04b5ec9780d9f4c0ce7e6
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2023-10-11 10:27:55 +0700

    create ui for menu context

--- src/components/ContactListItem/index.tsx
+++ src/components/ContactListItem/index.tsx
@@ -3,6 +3,7 @@
 import { Contact } from "@/services/contact/types";
 import { css } from "@emotion/react";
 import ConditionalRender from "../ConditionalRender";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 type ContactListItemProps = {
   contact: Contact;
@@ -17,35 +18,60 @@ const ContactListItem = ({ contact }: ContactListItemProps) => {
   const hasPhoneNumber = phones?.length > 0;
   return (
     <div css={contactListItemStyle}>
-      <div css={nameWrapperStyle}>
-        <ConditionalRender condition={!!firstName}>
-          <span>{firstName}</span>
-        </ConditionalRender>
-        <ConditionalRender condition={!!lastName}>
-          <span>{lastName}</span>
-        </ConditionalRender>
-        <ConditionalRender condition={!firstName && !lastName}>
-          <ContactNameFallback text="name" />
-        </ConditionalRender>
-      </div>
       <div>
-        <div css={phoneNumberWrapper}>
-          <ConditionalRender
-            condition={!!hasPhoneNumber}
-            fallback={<ContactNameFallback text="phone number" />}
-          >
-            {hasPhoneNumber && (
-              <span>
-                {phones?.[0].number.length > 17
-                  ? `${phones?.[0].number.substring(0, 17)}...`
-                  : phones?.[0].number}
-              </span>
-            )}
+        <div css={nameWrapperStyle}>
+          <ConditionalRender condition={!!firstName}>
+            <span>{firstName}</span>
           </ConditionalRender>
-          <ConditionalRender condition={phones?.length > 1}>
-            <span css={hasMorePhoneBadgeStyle}>{`+${phones?.length - 1}`}</span>
+          <ConditionalRender condition={!!lastName}>
+            <span>{lastName}</span>
+          </ConditionalRender>
+          <ConditionalRender condition={!firstName && !lastName}>
+            <ContactNameFallback text="name" />
           </ConditionalRender>
         </div>
+        <div>
+          <div css={phoneNumberWrapper}>
+            <ConditionalRender
+              condition={!!hasPhoneNumber}
+              fallback={<ContactNameFallback text="phone number" />}
+            >
+              {hasPhoneNumber && (
+                <span>
+                  {phones?.[0].number.length > 17
+                    ? `${phones?.[0].number.substring(0, 17)}...`
+                    : phones?.[0].number}
+                </span>
+              )}
+            </ConditionalRender>
+            <ConditionalRender condition={phones?.length > 1}>
+              <span css={hasMorePhoneBadgeStyle}>{`+${
+                phones?.length - 1
+              }`}</span>
+            </ConditionalRender>
+          </div>
+        </div>
+      </div>
+      <div>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger css={actionTriggerButton}>
+            ≡
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content css={actionContentStyle} align="end">
+            <DropdownMenu.Item css={actionContentItemStyle} onSelect={() => {}}>
+              Add To Favorite
+            </DropdownMenu.Item>
+            <DropdownMenu.Item css={actionContentItemStyle} onSelect={() => {}}>
+              View Detail
+            </DropdownMenu.Item>
+            <DropdownMenu.Item css={actionContentItemStyle} onSelect={() => {}}>
+              Edit
+            </DropdownMenu.Item>
+            <DropdownMenu.Item css={actionContentItemStyle} onSelect={() => {}}>
+              Delete
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </div>
     </div>
   );
@@ -58,6 +84,9 @@ const nameWrapperStyle = css`
 `;
 
 const contactListItemStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 10px;
   border-bottom: 1px solid #e0e0e0;
   cursor: pointer;
@@ -82,6 +111,37 @@ const hasMorePhoneBadgeStyle = css`
   padding: 2px 4px;
   border-radius: 999px;
   background-color: #f3f4f6;
+`;
+
+const actionTriggerButton = css`
+  border: none;
+  font-size: 20px;
+  padding: 2px 10px;
+  border-radius: 8px;
+  font-weight: 600;
+  color: #374151;
+  cursor: pointer;
+  &:hover {
+    background-color: #e5e7eb;
+  }
+`;
+
+const actionContentStyle = css`
+  width: 150px;
+  border-radius: 8px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+const actionContentItemStyle = css`
+  padding: 8px 12px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+  &:hover {
+    color: #374151;
+    background-color: #f3f4f6;
+  }
 `;
 
 export default ContactListItem;
