0de0a7325e17425436041ecbd47f0efb8765fd5f
Ulil Albab Surya Negara <surya.negara@gontor.ac.id>
2023-10-15 13:11:48 +0700

    modify viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,18 @@ import { Inter } from "next/font/google";
 import BaseProviders from "@/components/Providers/ClientProviders";
 import RootStyleRegistry from "./emotion";
 import { ReactNode } from "react";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Phone Book Project",
   description: "A Tokopedia Phone Book Project Assignment by Ulil Albab",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-      </Head>
       <html lang="en">
         <body className={inter.className}>
           <RootStyleRegistry>
